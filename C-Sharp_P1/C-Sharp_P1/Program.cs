c39945761fa0379bfc88c877b69f82e5c665e97a
MD-SAYEEDUR-RAHMAN <saidurnohan@gmail.com>
2023-08-27 03:50:18 +0600

    First file added with rename

---
+++ C-Sharp_P1/C-Sharp_P1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IntroductionToCSharp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to C#");
+            Main1();//Print both of output
+        }
+        static void Main1()
+        {
+            Console.WriteLine("Welcome to C# 1");
+        }
+
+    }
+}
