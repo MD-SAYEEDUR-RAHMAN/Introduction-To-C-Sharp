5f8630052fe22a7b7e0c740ffd5bd2cb22454373
MD-SAYEEDUR-RAHMAN <saidurnohan@gmail.com>
2023-11-13 12:43:07 +0600

    Add Abstract

---
+++ Abstract_P32/Abstract_P32/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_P32
+{
+    public abstract class Abs
+    {
+        public abstract void Method();
+        public void Method2() //contain method without abstract keyword
+        {
+            Console.WriteLine("Method implemented possible without abstract keyword");
+        }
+    }
+   
+    internal class Program:Abs //Non-Abstract class must implement all abstract method
+    {
+        public override void Method()
+        {
+            Console.WriteLine("Abstract method implement done");
+        }
+        static void Main(string[] args)
+        {   
+            Program p = new Program();//
+            p.Method();
+            p.Method2(); 
+            Abs ab =new Program(); //Parent reference variable can point to a derived class variable
+            ab.Method();
+            ab.Method2();
+        }
+    }
+}
